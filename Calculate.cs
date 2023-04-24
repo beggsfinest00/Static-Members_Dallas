924bd4ba83dded22571fb67296f93919f44ba726
beggsfinest00 <81796822+beggsfinest00@users.noreply.github.com>
2023-04-24 15:08:31 -0500

    Initial commit

---
+++ Calculate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Members_Dallas
+{
+    static class Calculate
+    {
+        //initilize and set the valurable to 0
+        public static float result = 0.0f;
+
+        //set the valurable to 0
+        static Calculate() 
+        {
+            result = 0.0f;
+        }
+
+        //INTEGERS that add, subtract, multipliy, and divide two numbers and ruturns the answer.
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+
+        //FLOATERS that add, subtract, multipliy, and divide two numbers and ruturns the answer.
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+
+        public static float Sub(float x, float y)
+        {
+            return x - y;
+        }
+
+        public static float Mult(float x, float y)
+        {
+            return x * y;
+        }
+
+        public static float Div(float x, float y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Static_Members_Dallas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prints integer methids to the console.
+            Console.WriteLine("Integer Methods");
+            //calls the Calculate classes methods on two number then prints answer to console as an int.
+            Console.WriteLine("15 + 88 is" + Calculate.Add(15, 88));
+            Console.WriteLine(Calculate.Sub(78, 88));
+            Console.WriteLine(Calculate.Mult(8, 9));
+            Console.WriteLine(Calculate.Div(77, 8));
+
+            //prints float methods to the console.
+            Console.WriteLine("\nFloat Methods");
+            //calls the Calculate classes methods on two number then prints answer to console as an Floats.
+            Console.WriteLine(Calculate.Add(15.55f, 88.65f));
+            Console.WriteLine(Calculate.Sub(78.45f, 88.89f));
+            Console.WriteLine(Calculate.Mult(8.99f, 9.65f));
+            Console.WriteLine(Calculate.Div(77.265f, 8.0f));
+
+        }
+    }
+}
